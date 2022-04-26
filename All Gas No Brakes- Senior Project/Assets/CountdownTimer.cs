c1a8d44d7300e2a32eb2cee2267fcb12b4cc9389
jp1700 <97564842+jp1700@users.noreply.github.com>
2022-04-26 14:52:19 -0500

    Timer
    Timer for the level
    60 seconds timer

---
+++ All Gas No Brakes- Senior Project/Assets/CountdownTimer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 60f;
+
+    public Text countdownText;
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;//decrease by 1 second
+        countdownText.text = currentTime.ToString("0");//format string to show whole number
+    }
+}
